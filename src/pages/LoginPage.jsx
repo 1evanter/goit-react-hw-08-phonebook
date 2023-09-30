f52399c3080f4eb11e4dd96a12dadd79e17d5e9c
1evanter <rita.oleksenkoo@gmail.com>
2023-09-30 13:01:04 +0300

    create Register & Login pages

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,11 @@
+import { LoginForm } from 'components/LoginForm/loginForm';
+
 const LoginPage = () => {
-  return <div>LOGIN PAGE</div>;
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,5 +1,11 @@
+import { RegisterForm } from 'components/RegisterForm/RegisterForm';
+
 const RegisterPage = () => {
-  return <div>REGISTER PAGE</div>;
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
 };
 
 export default RegisterPage;
