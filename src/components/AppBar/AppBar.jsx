ae8d08af0dde4e5d0ff2f7f01d66b8b2f1c3bc4d
1evanter <rita.oleksenkoo@gmail.com>
2023-09-30 14:12:43 +0300

    add condition to render

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,11 +1,15 @@
 import { Navigation } from 'components/Navigation/Navigation';
 import { AuthNav } from '../AuthNav/AuthNav';
+import { useAuth } from 'hooks/useAuth';
+import { UserMenu } from 'components/UserMenu/UserMenu';
 
 export const AppBar = () => {
+  const { isLoggedIn } = useAuth();
+
   return (
     <header>
       <Navigation />
-      <AuthNav />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
     </header>
   );
 };
