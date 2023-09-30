95f713aa1fe7772f84be059f0327a1f34d168368
1evanter <rita.oleksenkoo@gmail.com>
2023-09-30 20:32:51 +0300

    AppBar styles

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,14 +2,15 @@ import { Navigation } from 'components/Navigation/Navigation';
 import { AuthNav } from '../AuthNav/AuthNav';
 import { useAuth } from 'hooks/useAuth';
 import { UserMenu } from 'components/UserMenu/UserMenu';
+import { Header } from './AppBar.styled';
 
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
 
   return (
-    <header>
+    <Header>
       <Navigation />
       {isLoggedIn ? <UserMenu /> : <AuthNav />}
-    </header>
+    </Header>
   );
 };
