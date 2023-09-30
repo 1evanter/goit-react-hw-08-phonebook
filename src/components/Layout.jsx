c6abd86ff4430ce78d558e292028854c80836750
1evanter <rita.oleksenkoo@gmail.com>
2023-09-30 12:04:46 +0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import { AppBar } from './AppBar/AppBar';
+import { GlobalStyle } from './GlobalStyle';
 
 export const Layout = () => (
   <div>
     <AppBar />
     <Outlet />
+    <GlobalStyle />
   </div>
 );
