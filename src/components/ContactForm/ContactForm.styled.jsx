6fd2148b7abd7fa82ce33cb0f627b04ca7c8465f
1evanter <rita.oleksenkoo@gmail.com>
2023-09-30 21:55:23 +0300

    Update ContactForm.styled.jsx

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -12,25 +12,47 @@ export const StyledForm = styled(Form)`
   align-items: center;
   border: 3px dashed #526d82;
 `;
-export const StyledField = styled(Field)`
+
+export const Label = styled.label`
   display: flex;
   flex-direction: column;
+  font-weight: 500;
+`;
+
+export const StyledField = styled(Field)`
   margin-top: 3px;
-  border: 1px solid #526d82;
+  padding: 4px;
+
+  border: 2px solid #9db2bf;
   border-radius: 3px;
   outline: none;
-  padding: 4px;
+
+  transition: border 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover,
+  &:focus {
+    border: 2px solid #27374d;
+  }
 `;
 
 export const Button = styled.button`
-  color: #27374d;
-  background-color: #dde6ed;
-  border: 1px solid #526d82;
-  border-radius: 3px;
-  padding: 3px;
   min-width: 80px;
-`;
+  padding-top: 5px;
+  padding-bottom: 5px;
 
-export const Label = styled.label`
-  font-weight: 500;
+  color: #fff;
+  background-color: #27374d;
+  border: 2px solid #27374d;
+  border-radius: 3px;
+
+  transition: border 250ms cubic-bezier(0.4, 0, 0.2, 1),
+    background-color 250ms cubic-bezier(0.4, 0, 0.2, 1),
+    color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover,
+  &:focus {
+    border: 2px solid #526d82;
+    background-color: #dde6ed;
+    color: #27374d;
+  }
 `;
