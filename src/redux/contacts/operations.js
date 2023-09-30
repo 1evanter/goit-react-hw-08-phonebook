fbac86bf45715b7931b9414606149d1f2290e92e
1evanter <rita.oleksenkoo@gmail.com>
2023-09-30 12:30:40 +0300

    Update operations.js

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://6515c5e609e3260018c9202f.mockapi.io';
-
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchContacts',
   async (_, thunkAPI) => {
