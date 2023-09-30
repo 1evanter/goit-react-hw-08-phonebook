07484991ce3f2ca45f061b8e19e16f8f62bfb930
1evanter <rita.oleksenkoo@gmail.com>
2023-09-30 21:44:07 +0300

    RegisterForm styles

---
+++ src/components/RegisterForm/RegisterForm.styled.jsx
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  width: 320px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+
+  margin: 30px auto;
+  padding: 15px;
+  gap: 15px;
+  border: 3px dashed #526d82;
+`;
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Input = styled.input`
+  margin-top: 3px;
+  padding: 4px;
+
+  border: 2px solid #9db2bf;
+  border-radius: 3px;
+  outline: none;
+
+  transition: border 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover,
+  &:focus {
+    border: 2px solid #27374d;
+  }
+`;
+
+export const Button = styled.button`
+  display: block;
+  color: #fff;
+  background-color: #27374d;
+  border: 2px solid #27374d;
+  border-radius: 3px;
+  padding: 5px 66px;
+
+  transition: border 250ms cubic-bezier(0.4, 0, 0.2, 1),
+    background-color 250ms cubic-bezier(0.4, 0, 0.2, 1),
+    color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover,
+  &:focus {
+    border: 2px solid #526d82;
+    background-color: #dde6ed;
+    color: #27374d;
+  }
+`;
