d77c26aad705bb368d0939028b3888a4222c0978
1evanter <rita.oleksenkoo@gmail.com>
2023-09-30 22:13:27 +0300

    add button styles to GlobalStyles

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -28,5 +28,24 @@ a {
 
 button {
   cursor: pointer;
+   min-width: 80px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+
+  color: #fff;
+  background-color: #526d82;
+  border: 2px solid #526d82;
+  border-radius: 3px;
+
+  transition: border 250ms cubic-bezier(0.4, 0, 0.2, 1),
+    background-color 250ms cubic-bezier(0.4, 0, 0.2, 1),
+    color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover,
+  &:focus {
+    border: 2px solid #526d82;
+    background-color: #dde6ed;
+    color: #27374d;
+  }
 }
-`
+`;
