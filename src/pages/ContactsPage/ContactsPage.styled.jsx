5daad28aafda0ea31bb7645a3a69e0d3c46e5eba
1evanter <rita.oleksenkoo@gmail.com>
2023-10-01 16:21:41 +0300

    Update ContactsPage.styled.jsx

--- src/pages/ContactsPage/ContactsPage.styled.jsx
+++ src/pages/ContactsPage/ContactsPage.styled.jsx
@@ -23,6 +23,25 @@ export const Section = styled.section`
   width: 360px;
   height: 450px;
   padding: 15px;
-  border: 3px dashed #526d82;
+
   overflow: auto;
+
+  &::-webkit-scrollbar {
+    width: 4px;
+    height: 6px;
+  }
+  &::-webkit-scrollbar-track {
+    border-radius: 10px;
+    background: rgba(0, 0, 0, 0.1);
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background: rgba(0, 0, 0, 0.2);
+  }
+  &::-webkit-scrollbar-thumb:hover {
+    background: rgba(0, 0, 0, 0.4);
+  }
+  &::-webkit-scrollbar-thumb:active {
+    background: rgba(0, 0, 0, 0.6);
+  }
 `;
