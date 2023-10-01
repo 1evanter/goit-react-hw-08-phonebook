623253b2995242a5a630daa96fb200a7a11cd32a
1evanter <rita.oleksenkoo@gmail.com>
2023-10-01 16:21:39 +0300

    Update ContactForm.styled.jsx

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -10,7 +10,13 @@ export const StyledForm = styled(Form)`
   padding: 65px 15px;
   gap: 15px;
   align-items: center;
-  border: 3px dashed #526d82;
+  border-radius: 4px;
+  background-image: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 0.1) 0%,
+    rgba(255, 255, 255, 0.4) 100%
+  );
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 `;
 
 export const Label = styled.label`
