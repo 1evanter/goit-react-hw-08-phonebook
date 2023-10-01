b44b093c502e6e28003fd395d15eb708d9dabf36
1evanter <rita.oleksenkoo@gmail.com>
2023-10-01 14:05:20 +0300

    Update Filter.styled.jsx

--- src/components/Filter/Filter.styled.jsx
+++ src/components/Filter/Filter.styled.jsx
@@ -1,25 +1,28 @@
 import styled from 'styled-components';
 
 export const Form = styled.form`
-  max-width: 320px;
+  max-width: 360px;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   align-items: center;
   margin-bottom: 25px;
   gap: 15px;
+  max-height: 65px;
 `;
 
 export const Label = styled.label`
   display: flex;
   flex-direction: column;
   font-weight: 500;
+  font-size: 20px;
+  text-align: center;
 `;
 
 export const Input = styled.input`
   display: flex;
   flex-direction: column;
-  margin-top: 3px;
+  margin-top: 8px;
   border: 2px solid #9db2bf;
   border-radius: 3px;
   outline: none;
