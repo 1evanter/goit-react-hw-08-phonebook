9b14f9058109edd34d8078dcd2a3bb1149adf2e2
1evanter <rita.oleksenkoo@gmail.com>
2023-10-02 12:17:36 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,4 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import { contactsReducer } from './contacts/contactsSlice';
-// import { filterReducer } from './contacts/filterSlice';
-// import { authReducer } from './auth/authSlice';
-
-// export const store = configureStore({
-//   reducer: {
-//     contacts: contactsReducer,
-//     filter: filterReducer,
-//     auth: authReducer,
-//   },
-// });
-
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import {
   persistStore,
   persistReducer,
